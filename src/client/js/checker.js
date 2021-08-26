ddc3216927116f60cd98e93fb42b1c2bce41f148
Caprii31 <44691678+Caprii31@users.noreply.github.com>
2021-08-25 18:22:13 -0700

    commit

--- src/client/js/checker.js
+++
@@ -1,8 +0,0 @@
-//check if URL is valid
-
-function checkIfURL(inputText) {
-    const text =/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
-    return text.test(inputText)
-}
-
-export { checkIfURL }
